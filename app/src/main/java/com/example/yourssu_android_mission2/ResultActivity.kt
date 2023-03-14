08f35ccd0a4cb81eb8fa164c1ea6c2484ed34c3f
Yujin <jini1514@soongsil.ac.kr>
2023-03-15 02:25:24 +0900

    feat : result intent로 받아서 띄우기

--- app/src/main/java/com/example/yourssu_android_mission2/ResultActivity.kt
+++ app/src/main/java/com/example/yourssu_android_mission2/ResultActivity.kt
@@ -2,10 +2,18 @@ package com.example.yourssu_android_mission2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yourssu_android_mission2.databinding.ActivityEmailBinding
+import com.example.yourssu_android_mission2.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvEmail.text=intent.getStringExtra("email")
+        binding.tvPassword.text=intent.getStringExtra("pw")
     }
 }
